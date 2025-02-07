7bd1a3935f5cdef52300b621017eee664cea476d
gpng <geraldpng@gmail.com>
2025-02-07 09:36:29 +0800

    feat: migrate from ts-node to tsx

--- src/scripts/with-env.ts
+++
@@ -1,6 +0,0 @@
-// this file is solely to load the .env file before any other scripts are run.
-// This is needed because the dotenv.config() call is not hoisted to the top of the file.
-// Usage: ts-node -r ./src/scripts/with-env.ts src/scripts/create-migration.ts
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
